50808658d5d89af92c7ad4d09ae5799cbfc8658c
4lph4-dvlp <alpha030520@gmail.com>
2026-02-08 20:25:07 +0900

    Changed structure

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,8 +1,11 @@
 return {
   {
     "williamboman/mason.nvim",
-    config = function() require("mason").setup() end,
+    config = function()
+      require("mason").setup()
+    end,
   },
+
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
@@ -12,26 +15,49 @@ return {
       })
     end,
   },
+
   {
     "neovim/nvim-lspconfig",
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require('lspconfig')
-      
-      -- Setup servers with capabilities
-      local servers = { "lua_ls", "pyright", "ts_ls", "clangd" }
-      for _, server in ipairs(servers) do
-        lspconfig[server].setup({ capabilities = capabilities })
-      end
 
-      -- Key Bindings
+      -- 🔹 Neovim 0.11 방식 LSP 설정
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("pyright", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("ts_ls", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("clangd", {
+        capabilities = capabilities,
+      })
+
+      -- 🔹 LSP 활성화 (중요!)
+      vim.lsp.enable({
+        "lua_ls",
+        "pyright",
+        "ts_ls",
+        "clangd",
+      })
+
+      -- 🔹 Key Bindings
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      
-      -- Diagnostic UI
+
+      -- 🔹 Diagnostic UI
       vim.diagnostic.config({
-        virtual_text = true, signs = true, underline = true, severity_sort = true,
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
       })
     end,
   },
