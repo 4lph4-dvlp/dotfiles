bfde7be693bb1549f1703b0ccba006b3ccec678d
4lph4-dvlp <alpha030520@gmail.com>
2026-02-25 23:41:59 +0900

    Edit nvim vimconfig.lua

--- nvim/.config/nvim/lua/vimconfig.lua
+++ nvim/.config/nvim/lua/vimconfig.lua
@@ -1,9 +1,22 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader= " "
+-- 기본 설정
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.g.mapleader = " "
 
 -- Enable line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- 🔥 모든 언어에서 2칸 강제 (filetype 설정 덮어쓰기)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.indentexpr = ""  -- 언어별 indent 규칙 무시
+  end,
+})
